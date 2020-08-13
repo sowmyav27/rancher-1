2b40f281cf6c9a562d157777719280cf170b0d3e
sowmyav27 <viswanathan.sowmya@gmail.com>
2020-08-13 16:50:10 -0700

    Adding additional validation tests for hosted EKS cluster

--- tests/validation/tests/v3_api/test_hosted_eks_cluster.py
+++ tests/validation/tests/v3_api/test_hosted_eks_cluster.py
@@ -1,6 +1,6 @@
 import os
 from .common import get_user_client, validate_cluster, \
-    random_test_name, AWS_SSH_KEY_NAME
+    random_test_name, AWS_SSH_KEY_NAME, wait_for_cluster_delete
 from .test_create_ha import resource_prefix
 from lib.aws import AmazonWebServices
 import pytest
@@ -16,6 +16,7 @@ LOGGING_TYPES = os.environ.get('RANCHER_LOGGING_TYPES', None)
 EKS_SERVICE_ROLE = os.environ.get('RANCHER_EKS_SERVICE_ROLE', None)
 EKS_SUBNETS = os.environ.get('RANCHER_EKS_SUBNETS', None)
 EKS_SECURITYGROUP = os.environ.get('RANCHER_EKS_SECURITYGROUP', None)
+AWS_SSH_KEY_NAME = os.environ.get("AWS_SSH_KEY_NAME")
 EKS_PUBLIC_ACCESS_SOURCES = \
     os.environ.get('RANCHER_EKS_PUBLIC_ACCESS_SOURCES', None)
 ekscredential = pytest.mark.skipif(not (EKS_ACCESS_KEY and EKS_SECRET_KEY),
@@ -48,12 +49,8 @@ eks_config = {
 
 @ekscredential
 def test_eks_v2_create_hosted_cluster_1():
-    ec2_cloud_credential = get_aws_cloud_credential()
     cluster_name = random_test_name("test-auto-eks")
-    global eks_config
-    eks_config_temp = eks_config.copy()
-    eks_config_temp["displayName"] = cluster_name
-    eks_config_temp["amazonCredentialSecret"] = ec2_cloud_credential.id
+    eks_config_temp = get_eks_config_basic(cluster_name)
     cluster_config = {
         "eksConfig": eks_config_temp,
         "name": cluster_name,
@@ -68,28 +65,8 @@ def test_eks_v2_create_hosted_cluster_1():
 
 @ekscredential
 def test_eks_v2_create_hosted_cluster_2():
-    ec2_cloud_credential = get_aws_cloud_credential()
     cluster_name = random_test_name("test-auto-eks")
-    global eks_config
-    public_access = [] if EKS_PUBLIC_ACCESS_SOURCES \
-                          is None else EKS_PUBLIC_ACCESS_SOURCES.split(",")
-    eks_config_temp = eks_config.copy()
-    eks_config_temp["displayName"] = cluster_name
-    eks_config_temp["amazonCredentialSecret"] = ec2_cloud_credential.id
-    eks_config_temp["kmsKey"] = KMS_KEY
-    eks_config_temp["secretsEncryption"] = SECRET_ENCRYPTION
-    eks_config_temp["subnets"] = [] \
-        if EKS_SUBNETS is None else EKS_SUBNETS.split(",")
-    eks_config_temp["securityGroups"] = [] \
-        if EKS_SECURITYGROUP is None else EKS_SECURITYGROUP.split(",")
-    eks_config_temp["publicAccessSources"] = public_access
-    eks_config_temp["tags"] = {"cluster-level": "tag1"}
-    eks_config_temp["nodeGroups"][0]["tags"] = \
-        {"nodegroup-level": "tag1", "nodegroup-level": "tag2"}
-    eks_config_temp["nodeGroups"][0]["labels"] = {"label1": "value1"}
-    eks_config_temp["loggingTypes"] = get_logging_types()
-    eks_config_temp["serviceRole"] = EKS_SERVICE_ROLE
-    eks_config_temp["ec2SshKey"] = AWS_SSH_KEY_NAME
+    eks_config_temp = get_eks_config_all(cluster_name)
     cluster_config = {
         "eksConfig": eks_config_temp,
         "name": cluster_name,
@@ -100,6 +77,44 @@ def test_eks_v2_create_hosted_cluster_2():
         "enableClusterMonitoring": False
     }
     create_and_validate_eks_cluster(cluster_config)
+    validate_eks_cluster(cluster_name, eks_config_temp, all_parameters=True)
+
+
+@ekscredential
+def test_create_hosted_eks_cluster_3():
+    cluster_name = random_test_name("test-auto-eks")
+    eks_config_temp = get_eks_config_basic(cluster_name)
+    cluster_config = {
+        "eksConfig": eks_config_temp,
+        "name": cluster_name,
+        "type": "cluster",
+        "dockerRootDir": "/var/lib/docker",
+        "enableNetworkPolicy": False,
+        "enableClusterAlerting": False,
+        "enableClusterMonitoring": False
+    }
+    client, cluster = create_and_validate_eks_cluster(cluster_config)
+    # edit cluster
+    cluster = edit_and_validate_eks_cluster(cluster, eks_config_temp)
+
+
+@ekscredential
+def test_create_hosted_eks_cluster_4():
+    cluster_name = random_test_name("test-auto-eks")
+    eks_config_temp = get_eks_config_basic(cluster_name)
+    cluster_config = {
+        "eksConfig": eks_config_temp,
+        "name": cluster_name,
+        "type": "cluster",
+        "dockerRootDir": "/var/lib/docker",
+        "enableNetworkPolicy": False,
+        "enableClusterAlerting": False,
+        "enableClusterMonitoring": False
+    }
+    client, cluster = create_and_validate_eks_cluster(cluster_config)
+    # delete cluster
+    client.delete(cluster)
+    wait_for_cluster_delete(client, cluster)
 
 
 @ekscredential
@@ -186,4 +201,133 @@ def get_logging_types():
         temp = LOGGING_TYPES.split(",")
         for logging in temp:
             logging_types.append(logging)
+    else:
+        logging_types=["audit","api","authenticator"]
     return logging_types
+
+
+def get_eks_config_basic(cluster_name):
+    ec2_cloud_credential = get_aws_cloud_credential()
+    global eks_config
+    eks_config_temp = eks_config.copy()
+    eks_config_temp["displayName"] = cluster_name
+    eks_config_temp["amazonCredentialSecret"] = ec2_cloud_credential.id
+    return eks_config_temp
+
+
+def get_eks_config_all(cluster_name):
+    ec2_cloud_credential = get_aws_cloud_credential()
+    global eks_config
+    public_access = [] if EKS_PUBLIC_ACCESS_SOURCES \
+                          is None else EKS_PUBLIC_ACCESS_SOURCES.split(",")
+    eks_config_temp = eks_config.copy()
+    eks_config_temp["displayName"] = cluster_name
+    eks_config_temp["amazonCredentialSecret"] = ec2_cloud_credential.id
+    if KMS_KEY is not None: eks_config_temp["kmsKey"] = KMS_KEY
+    if SECRET_ENCRYPTION: eks_config_temp["secretsEncryption"] = \
+        SECRET_ENCRYPTION
+    eks_config_temp["subnets"] = [] \
+        if EKS_SUBNETS is None else EKS_SUBNETS.split(",")
+    eks_config_temp["securityGroups"] = [] \
+        if EKS_SECURITYGROUP is None else EKS_SECURITYGROUP.split(",")
+    eks_config_temp["publicAccessSources"] = public_access
+    eks_config_temp["tags"] = {"cluster-level": "tag1"}
+    eks_config_temp["nodeGroups"] = []
+    eks_config_temp["nodeGroups"].append(get_new_node())
+    eks_config_temp["nodeGroups"][0]["tags"] = \
+        {"nodegroup-level": "tag1", "nodegroup-level": "tag2"}
+    eks_config_temp["nodeGroups"][0]["labels"] = {"label1": "value1"}
+    eks_config_temp["loggingTypes"] = get_logging_types()
+    eks_config_temp["serviceRole"] = EKS_SERVICE_ROLE
+    eks_config_temp["ec2SshKey"] = AWS_SSH_KEY_NAME
+    return eks_config_temp
+
+
+def get_new_node():
+    new_nodegroup = {
+        "desiredSize": EKS_NODESIZE,
+        "diskSize": 20,
+        "gpu": False,
+        "instanceType": "t3.medium",
+        "maxSize": EKS_NODESIZE,
+        "minSize": EKS_NODESIZE,
+        "nodegroupName": random_test_name("test-ng"),
+        "ec2SshKey": AWS_SSH_KEY_NAME.split(".pem")[0],
+        "type": "nodeGroup"
+    }
+    return new_nodegroup
+
+
+def validate_eks_cluster(cluster_name, eks_config_temp, all_parameters=False):
+    eks_cluster = AmazonWebServices().describe_eks_cluster(cluster_name)
+    print("\nEKS cluster deployed in EKS Console: {}".
+          format(eks_cluster["cluster"]))
+    assert eks_cluster["cluster"]["version"] == \
+           eks_config_temp["kubernetesVersion"], "K8s version is incorrect"
+    assert eks_cluster["cluster"]["status"] == "ACTIVE", \
+        "Cluster is NOT in active state"
+    nodegroups = eks_config["nodeGroups"]
+    for nodegroup in nodegroups:
+        print("nodegroup:", nodegroup)
+        eks_nodegroup = AmazonWebServices().describe_eks_nodegroup(
+            cluster_name, nodegroup["nodegroupName"]
+        )
+        print("\nNode Group from EKS console: {}".format(eks_nodegroup))
+
+    if eks_config_temp["securityGroups"] is not None:
+        eks_cluster["cluster"]["resourcesVpcConfig"]
+        ["securityGroupIds"].sort()
+        eks_config_temp["securityGroups"].sort()
+        assert eks_cluster["cluster"]["resourcesVpcConfig"]
+        ["securityGroupIds"] == \
+               eks_config_temp["securityGroups"] , \
+        "Mismatch in Security Groups"
+    if eks_config_temp["subnets"] is not None:
+        eks_config_temp["subnets"].sort()
+        eks_cluster["cluster"]["resourcesVpcConfig"]
+        ["subnetIds"].sort()
+        assert eks_cluster["cluster"]["resourcesVpcConfig"]
+        ["subnetIds"] == \
+               eks_config_temp["subnets"], "Mismatch in Security Groups"
+    # verify logging types
+    if eks_config_temp["cluster"]["loggingTypes"] is not None:
+        assert eks_cluster["cluster"]["logging"]["clusterLogging"]["types"]\
+               == eks_config_temp["cluster"]["loggingTypes"] , \
+            "Mismatch in Logging types set"
+
+
+def edit_and_validate_eks_cluster(cluster, eks_config_temp):
+    # edit eks_config_temp
+    # add new cloud cred
+    ec2_cloud_credential_new = get_aws_cloud_credential()
+    eks_config_temp["amazonCredentialSecret"] = ec2_cloud_credential_new.id
+    # add cluster level tags
+    eks_config_temp["tags"] = {"cluster-level-2": "tag2"}
+    # add node group
+    new_nodegroup = get_new_node()
+    eks_config_temp["nodeGroups"].append(new_nodegroup)
+    # remove all logging
+    eks_config_temp["loggingTypes"] = get_logging_types()
+    client = get_user_client()
+    client.update(cluster, name=cluster.name, eksConfig=eks_config_temp)
+    cluster = validate_cluster(client, cluster, intermediate_state="updating",
+                               check_intermediate_state=True,
+                               skipIngresscheck=True,
+                               timeout=DEFAULT_TIMEOUT_EKS)
+    eks_cluster = AmazonWebServices().describe_eks_cluster(cluster.name)
+    print("\nEKS cluster deployed in EKS Console: {}".
+          format(eks_cluster["cluster"]))
+    nodegroups = eks_config["nodeGroups"]
+    for nodegroup in nodegroups:
+        print("nodegroup:", nodegroup)
+        eks_nodegroup = AmazonWebServices().describe_eks_nodegroup(
+            cluster.name, nodegroup["nodegroupName"]
+        )
+
+    return cluster
+
+
+def validate_nodegroup(nodegroup, eks_nodegroup):
+    assert nodegroup["ec2SshKey"] == eks_nodegroup["ec2SshKey"], \
+        "Ssh key is incorrect on the nodes"
+
